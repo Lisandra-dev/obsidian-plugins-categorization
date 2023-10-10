917194c130e7fc1b46978ab64ad45f48fc9170f9
Mara <mara-li@outlook.fr>
2023-10-10 23:18:49 +0200

    feat: add date check

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 
 from interface import PluginItems
+from seatable_api.date_utils import dateutils
 
 
 def generate_activity_tag(plugin: PluginItems) -> str:
     if plugin.last_commit_date:
-        today = datetime.today()
-        if isinstance(plugin.last_commit_date, str):
-            last_commit_date = datetime.strptime(plugin.last_commit_date, "%Y-%m-%d")
-        else:
-            last_commit_date = plugin.last_commit_date
-        diff_time = today - last_commit_date
-        diff_days = diff_time.days
-        if diff_days < 365:  # noqa
+        today = dateutils.now()
+        last_commit_date = plugin.last_commit_date
+        if isinstance(last_commit_date, datetime):
+            last_commit_date = last_commit_date.isoformat()
+        diff_time = dateutils.datediff(last_commit_date, today, unit="D")
+
+        if diff_time and diff_time < 365:  # noqa
             return "ACTIVE"
         else:
             return "STALE"
