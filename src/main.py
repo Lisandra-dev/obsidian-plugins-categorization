582e1141a35167549b1c53f2021b380c46212c5e
Mara <mara-li@outlook.fr>
2023-10-10 23:18:40 +0200

    feat: add delete plugin

--- src/main.py
+++ src/main.py
@@ -3,11 +3,17 @@ import os
 import sys
 
 import pandas as pd
-from database.search import get_etags_by_plugins
+from database.add_new import add_new
+from database.search import (
+    get_etags_by_plugins,
+    plugin_is_in_database,
+    search_deleted_plugin,
+)
 from database.update import update_old_entry
 from dotenv import load_dotenv
 from get_plugins import get_raw_data
 from github import Auth, Github
+from interface import PluginItems
 from rich import print
 from rich.console import Console
 from seatable_api import Base
@@ -34,7 +40,8 @@ def main() -> None:
     start_time = datetime.datetime.now()
     # get rate limit
     max_length: int | None = None
-    if len(sys.argv) > 1 and sys.argv[1] == "dev":
+    dev = len(sys.argv) > 1 and sys.argv[1] == "dev"
+    if dev:
         max_length = 5
     rate_limit = octokit.get_rate_limit()
     print(f"Rate limit: {rate_limit.core.remaining}/{rate_limit.core.limit}")
@@ -46,13 +53,42 @@ def main() -> None:
 
     with console.status("[bold green]Fetching data from GitHub", spinner="dots"):
         all_plugins = get_raw_data(octokit, commits_from_db, max_length=max_length)  # noqa
-
     console.log(f"Found {len(all_plugins)} plugins on GitHub")
+    if dev:
+        test_plugin = {
+            "id": "test",
+            "name": "test",
+            "description": "test",
+            "repo": "test",
+            "author": "test",
+            "fundingUrl": "test",
+            "isDesktopOnly": False,
+            "last_commit_date": datetime.datetime.now(),
+            "etag": "test",
+            "status": "ACTIVE",
+        }
+
+        all_plugins.append(PluginItems(**test_plugin))
 
     with console.status("[bold green]Updating database", spinner="dots"):
         for plugin in all_plugins:
-            console.print(f"• Updating {plugin.name}")
-            update_old_entry(plugin, db, base, console)
+            if plugin_is_in_database(db, plugin):
+                console.print(f"• Updating {plugin.name}")
+                update_old_entry(plugin, db, base, console)
+            else:
+                console.print(f"• Adding {plugin.name}")
+                add_new(plugin, base)
+
+    with console.status("[bold red]Searching for deleted plugins", spinner="dots"):
+        deleted_plugins = search_deleted_plugin(db, all_plugins, max_length=max_length)
+        if deleted_plugins:
+            console.log(f"Found {len(deleted_plugins)} deleted plugins")
+            for plugin in deleted_plugins:
+                if not dev:
+                    base.delete_row("Plugins", plugin["_id"])
+
+        else:
+            console.log("No deleted plugins found")
 
     end_time = datetime.datetime.now()
     diff_time_in_min = (end_time - start_time).total_seconds() / 60
