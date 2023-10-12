86d330e6ffd521f16dacba673832e54f9ddcc2d0
Mara <mara-li@outlook.fr>
2023-10-12 19:32:55 +0200

    style

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,6 @@ def generate_activity_tag(plugin: PluginItems) -> str:
         if isinstance(last_commit_date, datetime):
             last_commit_date = last_commit_date.isoformat()
         diff_time = dateutils.datediff(last_commit_date, today, unit="D")
-
         if diff_time and diff_time < 365:  # noqa
             return "ACTIVE"
         else:
